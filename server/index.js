d13cc07866d30cdbd476940618300ffc21270535
Alexsvid <39583525+Alexsvid@users.noreply.github.com>
2018-06-07 17:37:58 +0300

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -3,10 +3,21 @@ var wss = require("ws").Server;
 var server = new wss({port : 591});
 
 var clients = new Set();
+var messages = [];
+
+var fs = require("fs");
+fs.readFile("server\\messages.txt", {flag:"a+"}, function(err, content) {
+    content;
+});
+
 server.on("connection", function(socket) {
     clients.add(socket);
+    for(var m of messages) {
+        socket.send(m);
+    }
 
     socket.on("message", function(message) {
+        messages.push(message);
         for(var inter of clients) {
             inter.send(message);
         }
